d9b88593b94520293ca4f5eb60815419abd71816
Anas Moussaif <anasmoussaif123@gmail.com>
2026-05-04 21:05:53 +0200

    feat(model): create empty UserPick POJO skeleton
    Add the package declaration and the empty class shell that will hold
    a single user provider selection. Comment explains the business rule:
    one provider per category.

---
+++ src/com/ayora/model/UserPick.java
@@ -0,0 +1,12 @@
+package com.ayora.model;
+
+/**
+ * Choix d'un prestataire par la mariee parmi ses recommandations.
+ *
+ * Une seule selection par categorie : la mariee retient UN salle,
+ * UN traiteur, UN photographe, etc.
+ */
+public class UserPick {
+
+	public UserPick() {}
+}
